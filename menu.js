ac101bbccb0a742caeca813baf64f086ea517edd
Javier <javier.saurin.martinez@gmail.com>
2026-05-12 13:28:37 +0200

    fix: transiciones funcionales y corregidas

--- menu.js
+++ menu.js
@@ -8,19 +8,63 @@ let juegoTarjeta = document.querySelector(".juegoTarjeta");
 let imagenTarjeta = document.querySelector(".imagenTarjeta");
 let descripcionTarjeta = document.querySelector(".descripcionTarjeta");
 let botonTarjeta = document.querySelector(".botonTarjeta");
+let tituloTarjeta = document.querySelector(".tituloTarjeta");
 
 // Evento click boton PPT //
 botonPPT.addEventListener("click", function() {
-    juegoTarjeta.classList.add("juegoTarjetaMostrar");
-    imagenTarjeta.src = "imagenes/PPT-imagen.png";
-    descripcionTarjeta.textContent = "El clásico Piedra, Papel o tijeras pero con una funcionalidad nueva, entra y ¡DESCÚBRELA!";
-    botonTarjeta.textContent = "Jugar YA";
+    if (juegoTarjeta.classList.contains("juegoTarjetaMostrar") === true) {
+        juegoTarjeta.classList.add("transicionSalidaTarjeta");
+        juegoTarjeta.addEventListener("animationend", function() {
+            if (event.animationName === 'transicionSalidaTarjeta') {
+                console.log("La animacion especifica ha terminado");
+                juegoTarjeta.classList.add("juegoTarjeta");
+                juegoTarjeta.classList.remove("transicionSalidaTarjeta");
+                juegoTarjeta.classList.remove("juegoTarjetaMostrar");
+            };
+        });
+        setTimeout(function() {
+            juegoTarjeta.classList.remove("juegoTarjetaMostrar");
+            juegoTarjeta.classList.remove("transicionSalidaTarjeta");
+            tituloTarjeta.textContent = "Piedra, Papel o Tijeras";
+            imagenTarjeta.src = "imagenes/PPT-imagen.png";
+            descripcionTarjeta.textContent = "El clásico Piedra, Papel o tijeras pero con una funcionalidad nueva, entra y ¡DESCÚBRELA!";
+            botonTarjeta.textContent = "Jugar YA";
+            juegoTarjeta.classList.add("juegoTarjetaMostrar");
+        }, 1500);
+    } else {juegoTarjeta.classList.add("juegoTarjetaMostrar")
+        tituloTarjeta.textContent = "Piedra, Papel o Tijeras";
+        imagenTarjeta.src = "imagenes/PPT-imagen.png";
+        descripcionTarjeta.textContent = "El clásico Piedra, Papel o tijeras pero con una funcionalidad nueva, entra y ¡DESCÚBRELA!";
+        botonTarjeta.textContent = "Jugar YA";
+    };
 });
 
 // Evento click boton Adivina el Numero //
 botonAdivinaNumero.addEventListener("click", function() {
-    juegoTarjeta.classList.add("juegoTarjetaMostrar");
-    imagenTarjeta.src = "imagenes/adivina-numero.png";
-    descripcionTarjeta.textContent = "Un adivina el número con tiempo límite, se ¡RÁPIDO! o no te dará tiempo a averiguarlo.";
-    botonTarjeta.textContent = "Jugar YA";
-})
+    if (juegoTarjeta.classList.contains("juegoTarjetaMostrar") === true) {
+        juegoTarjeta.classList.add("transicionSalidaTarjeta");
+        juegoTarjeta.addEventListener("animationend", function() {
+            if (event.animationName === 'transicionSalidaTarjeta') {
+                console.log("La animacion especifica ha terminado");
+                juegoTarjeta.classList.add("juegoTarjeta");
+                juegoTarjeta.classList.remove("transicionSalidaTarjeta");
+                juegoTarjeta.classList.remove("juegoTarjetaMostrar");
+            };
+        });
+        setTimeout(function() {
+            juegoTarjeta.classList.remove("juegoTarjetaMostrar");
+            juegoTarjeta.classList.remove("transicionSalidaTarjeta");
+            tituloTarjeta.textContent = "Adivina el número";
+            imagenTarjeta.src = "imagenes/adivina-numero.png";
+            descripcionTarjeta.textContent = "Sé más ¡RÁPIDO! que el tiempo para adivinar el número escondido entre 1 y 100";
+            botonTarjeta.textContent = "Jugar YA";
+            juegoTarjeta.classList.add("juegoTarjetaMostrar");
+        }, 1500);
+    } else {juegoTarjeta.classList.add("juegoTarjetaMostrar")
+        tituloTarjeta.textContent = "Adivina el número";
+        imagenTarjeta.src = "imagenes/adivina-numero.png";
+        descripcionTarjeta.textContent = "Sé más ¡RÁPIDO! que el tiempo para adivinar el número escondido entre 1 y 100";
+        botonTarjeta.textContent = "Jugar YA";
+    };
+});
+
