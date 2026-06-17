9ff8a9882934bbd2cc89f4a71ed07120c9bd2b24
Javier <javier.saurin.martinez@gmail.com>
2026-06-17 17:48:44 +0200

    fix: modal y animacion hecho

--- Ahorca2/ahorca2.js
+++ Ahorca2/ahorca2.js
@@ -1,4 +1,5 @@
 // Variables globales //
+let estructuraCentral = document.querySelector(".estructura-central");
 // Figura y cuerpo //
 let figura = document.querySelector(".figura");
 let cuerpo = document.querySelector(".cuerpo");
@@ -10,7 +11,7 @@ let piernaIzq = document.querySelector(".pierna-izq");
 let piernaDer = document.querySelector(".pierna-der");
 // Input letras //
 let conjuntoInput = document.querySelector(".conjunto-input");
-let inputLetras = document.querySelector(".input-letras");
+let inputLetras = document.querySelector(".input-letra");
 let adivinarLetra = document.querySelector(".adivinar-letra");
 let botonLetras = document.querySelector(".letras");
 // Input Adivinar Palabra //
@@ -33,3 +34,83 @@ let palabraGenerada = false;
 let muñecoCompleto = false;
 let palabraEnUso = null;
 let letrasExcluidas = [];
+
+// Arrays //
+let cuatroLetras = [
+  "casa",
+  "mesa",
+  "gato",
+  "lobo",
+  "peto",
+  "agua",
+  "vino",
+  "mapa",
+  "rojo",
+  "azul"
+];
+let cincoLetras = [
+  "coche",
+  "calle",
+  "amigo",
+  "chulo",
+  "libro",
+  "perro",
+  "noche",
+  "campo",
+  "verde",
+  "queso"
+];
+let seisLetras = [
+  "zapato",
+  "camisa",
+  "abuelo",
+  "escoba",
+  "fuente",
+  "puerta",
+  "gambas",
+  "bosque",
+  "ciudad",
+  "teatro"
+];
+
+// Evento comenzar el juego //
+botonGenerarPalabra.addEventListener("click", function() {
+    let overlayBody = document.createElement("div");
+    overlayBody.classList.add("overlayBody");
+    document.body.appendChild(overlayBody);
+
+    let menuElegirLetras = document.createElement("div");
+    menuElegirLetras.classList.add("menuLetras");
+    activarJuego.appendChild(menuElegirLetras);
+
+    let mensajeElegirLetras = document.createElement("p");
+    mensajeElegirLetras.textContent = "¿Cómo quieres jugar?";
+    mensajeElegirLetras.classList.add("mensajeElegirLetras");
+    menuElegirLetras.appendChild(mensajeElegirLetras);
+
+    let agruparBotonesMenu = document.createElement("div");
+    agruparBotonesMenu.classList.add("botonesMenuLetras");
+    menuElegirLetras.appendChild(agruparBotonesMenu);
+
+    let botonCuatroLetras = document.createElement("button");
+    botonCuatroLetras.textContent = "Palabras de 4 letras.";
+    botonCuatroLetras.classList.add("cuatroLetras");
+    agruparBotonesMenu.appendChild(botonCuatroLetras);
+
+    let botonCincoLetras = document.createElement("button");
+    botonCincoLetras.textContent = "Palabras de 5 letras.";
+    botonCincoLetras.classList.add("cincoLetras");
+    agruparBotonesMenu.appendChild(botonCincoLetras);
+
+    let botonSeisLetras = document.createElement("button");
+    botonSeisLetras.textContent = "Palabras de 6 letras.";
+    botonSeisLetras.classList.add("seisLetras");
+    agruparBotonesMenu.appendChild(botonSeisLetras);
+
+    let botonMezclarArrays = document.createElement("button");
+    botonMezclarArrays.textContent = "Al azar entre las tres listas";
+    botonMezclarArrays.classList.add("mezclarArrays");
+    agruparBotonesMenu.appendChild(botonMezclarArrays);
+
+    
+});
