d62aca3d50cbbc9099fc73170cd3ce6e0792803e
Javier <javier.saurin.martinez@gmail.com>
2026-07-02 17:41:58 +0200

    fix: juego ahorca-2 añadido al menu

--- menu.js
+++ menu.js
@@ -2,6 +2,7 @@
 // Botones de los juegos //
 let botonPPT = document.querySelector(".PPT");
 let botonAdivinaNumero = document.querySelector(".AdivinaNumero");
+let botonAhorcado = document.querySelector(".ahorcado");
 
 // Variables juego seleccionado //
 let juegoTarjeta = document.querySelector(".juegoTarjeta");
@@ -15,12 +16,14 @@ let tituloTarjeta = document.querySelector(".tituloTarjeta");
 botonPPT.addEventListener("click", function() {
     if (juegoTarjeta.classList.contains("juegoTarjetaMostrar") === true) {
         botonTarjeta.classList.remove("hover-adivina");
+        botonTarjeta.classList.remove("hover-ahorcado");
         botonTarjeta.classList.add("hover-ppt");
         juegoTarjeta.classList.add("transicionVuelta");
         juegoTarjeta.addEventListener("animationend", function() {
             if (event.animationName === 'transicionVuelta') {
                 console.log("La animacion especifica ha terminado");
                 botonTarjeta.classList.remove("hover-adivina");
+                botonTarjeta.classList.remove("hover-ahorcado");                
                 botonTarjeta.classList.add("hover-ppt");
                 juegoTarjeta.classList.add("juegoTarjeta");
                 juegoTarjeta.classList.remove("transicionVuelta");
@@ -39,6 +42,7 @@ botonPPT.addEventListener("click", function() {
         urlJuego = "P.P.T/index.html";
     } else {juegoTarjeta.classList.add("juegoTarjetaMostrar")
         botonTarjeta.classList.remove("hover-adivina");
+        botonTarjeta.classList.remove("hover-ahorcado");
         botonTarjeta.classList.add("hover-ppt");
         tituloTarjeta.textContent = "Piedra, Papel o Tijeras";
         imagenTarjeta.src = "imagenes/PPT-imagen.png";
@@ -61,12 +65,14 @@ botonTarjeta.addEventListener("click", function() {
 botonAdivinaNumero.addEventListener("click", function() {
     if (juegoTarjeta.classList.contains("juegoTarjetaMostrar") === true) {
         botonTarjeta.classList.remove("hover-ppt");
+        botonTarjeta.classList.remove("hover-ahorcado");
         botonTarjeta.classList.add("hover-adivina");
         juegoTarjeta.classList.add("transicionVuelta");
         juegoTarjeta.addEventListener("animationend", function() {
             if (event.animationName === 'transicionVuelta') {
                 console.log("La animacion especifica ha terminado");
                 botonTarjeta.classList.remove("hover-ppt");
+                botonTarjeta.classList.remove("hover-ahorcado");
                 botonTarjeta.classList.add("hover-adivina");
                 juegoTarjeta.classList.add("juegoTarjeta");
                 juegoTarjeta.classList.remove("transicionVuelta");
@@ -85,11 +91,51 @@ botonAdivinaNumero.addEventListener("click", function() {
         urlJuego = "Adivina el numero/index.html";
     } else {juegoTarjeta.classList.add("juegoTarjetaMostrar")
         botonTarjeta.classList.remove("hover-ppt");
+        botonTarjeta.classList.remove("hover-ahorcado");
         botonTarjeta.classList.add("hover-adivina");
         tituloTarjeta.textContent = "Adivina el número";
         imagenTarjeta.src = "imagenes/adivina-numero.png";
         descripcionTarjeta.textContent = "Sé más ¡RÁPIDO! que el tiempo para adivinar el número escondido entre 1 y 100";
         botonTarjeta.textContent = "Jugar YA";
         urlJuego = "Adivina el numero/index.html";
+    };
+});
+// Evento click boton ahorcado //
+botonAhorcado.addEventListener("click", function() {
+    if (juegoTarjeta.classList.contains("juegoTarjetaMostrar") === true) {
+        botonTarjeta.classList.remove("hover-ppt");
+        botonTarjeta.classList.remove("hover-adivina");
+        botonTarjeta.classList.add("hover-ahorcado");
+        juegoTarjeta.classList.add("transicionVuelta");
+        juegoTarjeta.addEventListener("animationend", function() {
+            if (event.animationName === 'transicionVuelta') {
+                console.log("La animacion especifica ha terminado");
+                botonTarjeta.classList.remove("hover-ppt");
+                botonTarjeta.classList.remove("hover-adivina");
+                botonTarjeta.classList.add("hover-ahorcado");
+                juegoTarjeta.classList.add("juegoTarjeta");
+                juegoTarjeta.classList.remove("transicionVuelta");
+                juegoTarjeta.classList.remove("juegoTarjetaMostrar");
+            };
+        });
+        setTimeout(function() {
+            juegoTarjeta.classList.remove("juegoTarjetaMostrar");
+            juegoTarjeta.classList.remove("transicionVuelta");
+            tituloTarjeta.textContent = "Ahorca-2";
+            imagenTarjeta.src = "imagenes/ahorcado.png";
+            descripcionTarjeta.textContent = "¿Serás capaz de adivinar la palabra secreta antes de acabar...💀 ¡Buena Suerte!";
+            botonTarjeta.textContent = "Jugar YA";
+            juegoTarjeta.classList.add("juegoTarjetaMostrar");
+        }, 1500);
+        urlJuego = "Ahorca2/ahorca2.html";
+    } else {juegoTarjeta.classList.add("juegoTarjetaMostrar")
+        botonTarjeta.classList.remove("hover-ppt");
+        botonTarjeta.classList.remove("hover-adivina");
+        botonTarjeta.classList.add("hover-ahorcado");
+        tituloTarjeta.textContent = "Ahorca-2";
+        imagenTarjeta.src = "imagenes/ahorcado.png";
+        descripcionTarjeta.textContent = "¿Serás capaz de adivinar la palabra secreta antes de acabar...💀 ¡Buena Suerte!";
+        botonTarjeta.textContent = "Jugar YA";
+        urlJuego = "Ahorca2/ahorca2.html";
     };
 });
