d4d3603af50e58b5ec7cdf0241794db3ad2d3e35
Javier <javier.saurin.martinez@gmail.com>
2026-07-08 19:52:27 +0200

    fix: serpiente aparece y responde a las teclas.

--- Snake/snake.js
+++ Snake/snake.js
@@ -9,3 +9,52 @@ let puntuacionMaxima = document.querySelector("#puntuacion-maxima");
 let leyenda = document.querySelector(".leyenda");
 const tablero = document.querySelector(".tablero");
 const pincel = tablero.getContext("2d");
+const tamanoCasilla = 40;
+let snake = [
+    {x: 12, y: 10},
+    {x: 11, y: 10},
+    {x: 10, y: 10}
+];
+let direccion = {x: 1, y: 0};
+// Funciones para movimientos serpiente //
+function dibujarSerpiente () {
+    pincel.fillStyle = "#124D1C";
+    snake.forEach(parteSerpiente => {
+        pincel.fillRect(parteSerpiente.x * tamanoCasilla, parteSerpiente.y * tamanoCasilla,
+            tamanoCasilla, tamanoCasilla
+        );
+    });
+}
+dibujarSerpiente();
+function moverSerpiente() {
+    const nuevaPosicionCabeza = {
+        x: snake[0].x + direccion.x,
+        y: snake[0].y + direccion.y
+    };
+    snake.unshift(nuevaPosicionCabeza);
+    snake.pop();
+}
+// Intervalo de tiempo para simular que se mueve la serpiente //
+setInterval(() => {
+    pincel.clearRect(0, 0, tablero.width, tablero.height);
+    moverSerpiente();
+    dibujarSerpiente();
+}, 150);
+
+// Evento movimiento teclas //
+window.addEventListener("keydown", (evento) => {
+    switch (evento.key) {
+        case "ArrowUp":
+            direccion = {x: 0, y: -1};
+            break;
+        case "ArrowDown":
+            direccion = {x: 0, y: 1};
+            break;
+        case "ArrowLeft":
+            direccion = {x: -1, y: 0};
+            break;
+        case "ArrowRight":
+            direccion = {x: 1, y: 0};
+            break;        
+    }
+})
