42a82f910689221f8a0b0fb642221ee19df5e20d
Javier <javier.saurin.martinez@gmail.com>
2026-07-09 20:19:56 +0200

    fix: manzanas aparecen aleatoriamente en el tablero, funcion de crecer cuerpo funcional a falta de intervalo de tiempo para la dorada.

--- Snake/snake.js
+++ Snake/snake.js
@@ -16,6 +16,36 @@ let snake = [
     {x: 10, y: 10}
 ];
 let direccion = {x: 1, y: 0};
+let manzanaRoja = {x: 5, y: 5};
+posicionManzana();
+let manzanaDorada = {x: 9, y: 9};
+posicionManzanaDorada();
+// Funcion pintar manzanas //
+function dibujarManzana () {
+    pincel.fillStyle = "#C62828";
+    pincel.fillRect(manzanaRoja.x * tamanoCasilla, manzanaRoja.y * tamanoCasilla,
+        tamanoCasilla, tamanoCasilla
+    );
+}
+function dibujarManzanaDorada () {
+    pincel.fillStyle = "#E4B028";
+    pincel.fillRect(manzanaDorada.x * tamanoCasilla, manzanaDorada.y * tamanoCasilla,
+        tamanoCasilla, tamanoCasilla
+    );
+}
+// Funcion posicion aleatoria manzana //
+function posicionManzana () {
+    manzanaRoja = {
+        x: Math.floor(Math.random()* (tablero.width/tamanoCasilla)),
+        y: Math.floor(Math.random()* (tablero.height/tamanoCasilla))
+    };
+}
+function posicionManzanaDorada () {
+    manzanaDorada = {
+        x: Math.floor(Math.random()* (tablero.width/tamanoCasilla)),
+        y: Math.floor(Math.random()* (tablero.height/tamanoCasilla))
+    };
+}
 // Funciones para movimientos serpiente //
 function dibujarSerpiente () {
     pincel.fillStyle = "#124D1C";
@@ -31,14 +61,25 @@ function moverSerpiente() {
         x: snake[0].x + direccion.x,
         y: snake[0].y + direccion.y
     };
-    snake.unshift(nuevaPosicionCabeza);
-    snake.pop();
+    if (manzanaRoja.x === snake[0].x && manzanaRoja.y === snake[0].y) {
+        snake.unshift(nuevaPosicionCabeza);
+        posicionManzana();
+    } else if (manzanaDorada.x === snake[0].x && manzanaDorada.y === snake[0].y) {
+        snake.unshift(nuevaPosicionCabeza);
+        snake.unshift(nuevaPosicionCabeza);        
+        posicionManzanaDorada();
+    } else {
+        snake.unshift(nuevaPosicionCabeza);
+        snake.pop();
+    }
 }
 // Intervalo de tiempo para simular que se mueve la serpiente //
 setInterval(() => {
     pincel.clearRect(0, 0, tablero.width, tablero.height);
     moverSerpiente();
     dibujarSerpiente();
+    dibujarManzana();
+    dibujarManzanaDorada();
 }, 150);
 
 // Evento movimiento teclas //
